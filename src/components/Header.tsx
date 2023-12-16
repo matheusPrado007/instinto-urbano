5df07bf4d632f14351ed936dd9b2144740e8d2a5
Prado <peu_prado01@hotmail.com>
2023-12-15 21:12:57 -0300

    feat:criando header e home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,30 @@
 // src/components/common/Header.tsx
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/Header.css';
 
 const Header: React.FC = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header>
-      {/* Conteúdo do header, como logotipo, links, etc. */}
+      <div className="menu-button" onClick={toggleMenu}>
+        ☰
+      </div>
+      <section className='head-1'>
       <h1>Rastro Urbano</h1>
-      <nav>
+      <nav className={isMenuOpen ? 'open' : ''}>
         <ul>
           <li><a href="/">Página Inicial</a></li>
           <li><a href="/sobre">Sobre</a></li>
-          {/* Adicione mais links conforme necessário */}
+          <li><a href="/artistas">Artistas</a></li>
+          <li><a href="/artes">Artes</a></li>
         </ul>
       </nav>
+      </section>
     </header>
   );
 };
