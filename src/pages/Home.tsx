e9dbc40b2588a4b87ee848b1d95164c05c193fb6
Prado <peu_prado01@hotmail.com>
2023-12-16 21:03:39 -0300

    fix: ajusta erro de upload, agora manda caminho de imagem para db

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,23 +3,27 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/Home.css'
+import ApiDataLoader from '../components/ApiDataLoader';
 
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <>
       <Header />
       <div className="container">
-      <p>Arte de rua.</p>
-      <div className="input-container">
-        <input type="text" className="inputField" placeholder="Pesquisar..." />
-        <div className="search-icon">
-        <img src="/lupa.png"  alt="Ícone de Lupa" />
+        <p>Arte de rua.</p>
+        <div className="input-container">
+          <input type="text" className="inputField" placeholder="Pesquisar..." />
+          <div className="search-icon">
+            <img src="/lupa.png" alt="Ícone de Lupa" />
+          </div>
         </div>
       </div>
-    </div>
+      <section>
+        <ApiDataLoader />
+      </section>
       <Footer />
-    </div>
+    </>
   );
 };
 
