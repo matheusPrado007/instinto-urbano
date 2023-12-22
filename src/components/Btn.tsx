573711f692dd1a3379ea0ed4d839a20be2d9655f
Prado <peu_prado01@hotmail.com>
2023-12-22 20:51:33 -0300

    feat: page about e router

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faArrowRight, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const CustomPrevArrow = (props: any) => (
   <div className="custom-prev-arrow" onClick={props.onClick}>
---
+++ src/pages/About.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import ApiDataLoader from '../services/ApiDataLoader';
+import '../styles/About.css';
+import logo from '../assets/logo.png';
+
+
+
+const About: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <section className='about-section'>
+        <p className='about-h2'> Rastro Urbano</p>
+        <p className='about-p'>
+          Onde as ruas ganham vida e a paixão pela arte urbana é celebrada. Em nosso santuário virtual,
+          criamos uma experiência envolvente que revela as emoções e visuais de cada obra nas ruas brasileiras.
+        </p>
+        <p className='about-p'>
+          Descubra o legado de artistas extraordinários que deixam seu "Rastro Urbano" por todo o país.
+          Nosso site é mais que uma plataforma; é um portal apaixonado que conecta admiradores à riqueza da
+          expressão artística urbana.
+        </p>
+        <p className='about-p'>
+          Dos grandes murais a artistas visionários, o "Rastro Urbano" destaca não apenas a localização
+          exata de cada obra-prima, mas também revela os bastidores, inspirações e sonhos que impulsionam
+          cada criação. Mergulhe nessa jornada onde cada clique revela a alma vibrante de nossas cidades.
+        </p>
+        <p className='about-p'>
+          Sinta a pulsação das ruas, apaixone-se pela criatividade que colore nosso cotidiano e deixe o
+          "Rastro Urbano" guiá-lo por um mundo de beleza efervescente. Este é mais que um site; é o convite
+          para explorar, se encantar e se perder na fascinante tapeçaria da cultura urbana brasileira.
+        </p>
+      </section>
+      <div className='container-logo'>
+      <img src={logo} alt="logo" className='logo-about' />
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default About;
