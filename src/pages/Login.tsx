1b2c306d9769b8af3305c361ef2f95f4b2f77f2e
Prado <peu_prado01@hotmail.com>
2023-12-26 01:24:11 -0300

    feat: loacl storage para email

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,64 +1,89 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Login.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { useApi } from '../services/context/ApiContext';
 import { useNavigate } from 'react-router-dom';
 
-
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [error, setError] = useState<string | null>(null);
+  const [saveEmail, setSaveEmail] = useState(false);
+
   const { fazerLogin, dadosUsers } = useApi();
+  const navigate = useNavigate();
 
-  const navigate = useNavigate(); 
-
-  const navigateToProfileAdm = () => {
-    const userId = dadosUsers.find((user) => user.email === email)._id
-    navigate(`/admuser/${userId}`); 
+  const navigateToProfileAdm = (userId: string) => {
+    navigate(`/admuser/${userId}`);
   };
 
-  const handleLogin = async ({ email, senha }: any) => {
+  useEffect(() => {
+    // Carregar o e-mail salvo do localStorage ao iniciar
+    const savedEmail = localStorage.getItem('savedEmail');
+    if (savedEmail) {
+      setEmail(savedEmail);
+      setSaveEmail(true);
+    }
+  }, []);
+
+  const handleLogin = async () => {
     try {
       const { accessToken, refreshToken } = await fazerLogin({ email, senha });
       if (accessToken && refreshToken) {
-        navigateToProfileAdm();
+        // Salvar o e-mail no localStorage se a opção estiver marcada
+        if (saveEmail) {
+          localStorage.setItem('savedEmail', email);
+        } else {
+          localStorage.removeItem('savedEmail');
+        }
+
+        // Navegar para o perfil do usuário
+        const userId = dadosUsers.find((user) => user.email === email)._id;
+        navigateToProfileAdm(userId);
       }
     } catch (error) {
       console.error('Erro durante o login:', error);
       setError('Erro ao fazer login. Verifique suas credenciais.');
     }
   };
-  
+
   return (
     <>
       <Header />
-      <div className='login-width'>
+      <div className="login-width">
         <div className="login-form">
           <label className="login-label">
-            Username:
+            Email:
             <input
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="login-input"
-              placeholder='Email'
+              placeholder="Email"
             />
           </label>
           <label className="login-label">
             Senha:
             <input
-              type="text"
+              type="password"
               value={senha}
               onChange={(e) => setSenha(e.target.value)}
               className="login-input"
-              placeholder='Senha'
+              placeholder="Senha"
             />
           </label>
-          <button onClick={() => handleLogin({email, senha}) } className="login-button"> 
+          <label className="login-label">
+            Salvar e-mail:
+            <input
+              type="checkbox"
+              checked={saveEmail}
+              onChange={() => setSaveEmail(!saveEmail)}
+            />
+          </label>
+          <button onClick={handleLogin} className="login-button">
             Login
-           </button>
+          </button>
           {error && <p className="login-error-message">{error}</p>}
         </div>
       </div>
