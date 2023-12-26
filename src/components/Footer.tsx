a31fcb5a35c8ab23997b13162cbd20c3c7b82ac1
Prado <peu_prado01@hotmail.com>
2023-12-26 03:07:07 -0300

    feat: imput checkbox bonito

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,13 @@ import '../styles/Footer.css';
 
 const Footer: React.FC = () => {
   return (
-    <footer>
+    <section className='footer'>
       <div className="container-footer clearfix">
         <div className="footer-content">
           <p>&copy; 2023 Matheus Prado. Todos os direitos reservados.</p>
         </div>
       </div>
-    </footer>
+    </section>
   );
 };
 
