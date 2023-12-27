2e6603922b9ff28283519acf1a31cfd7bd7a52bf
Prado <peu_prado01@hotmail.com>
2023-12-27 03:26:51 -0300

    feat: paginas login e começa perfil-edit

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import ArtList from './pages/Art';
 import ArtPage from './pages/ArtPage';
 import Login from './pages/Login';
 import AdmUser from './pages/AdmUser';
+import ProfileAdmin from './pages/ProfileAdmin';
+import ArtistAdmin from './pages/ArtistAdmin';
+import ArtAdmin from './pages/ArtAdmin';
 
 const AppRouter: React.FC = () => {
   return (
@@ -20,6 +23,9 @@ const AppRouter: React.FC = () => {
         <Route path="/arte/:artId" element={<ArtPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/admuser/:id" element={<AdmUser />} />
+        <Route path="/admuser/:id/perfil" element={<ProfileAdmin />} />
+        <Route path="/admuser/:id/artistas" element={<ArtistAdmin/>} />
+        <Route path="/admuser/:id/artes" element={<ArtAdmin />} />
       </Routes>
     </Router>
   );
