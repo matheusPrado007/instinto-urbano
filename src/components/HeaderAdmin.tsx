c4d952cb5a10116f2904b1ab6918090e7b0f8e78
Prado <peu_prado01@hotmail.com>
2024-01-03 14:31:11 -0300

    feat: menu some quando clicado em algo fora do menu

--- src/components/HeaderAdmin.tsx
+++ src/components/HeaderAdmin.tsx
@@ -6,31 +6,36 @@ import { useParams } from 'react-router-dom';
 const HeaderAdmin: React.FC = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
   const { id } = useParams<{ id?: string }>();
-  const menuRef = useRef(null);
+  const menuRef = useRef<HTMLDivElement | null>(null);
+  const buttonRef = useRef<HTMLDivElement | null>(null);
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
 
   const closeMenu = (event: MouseEvent) => {
-    if (menuRef.current && (menuRef.current as HTMLElement).contains(event.target as HTMLElement)) {
-      return;
+    if (menuRef.current && buttonRef.current) {
+      if (!menuRef.current.contains(event.target as Node) && !buttonRef.current.contains(event.target as Node)) {
+        setMenuOpen(false);
+      }
     }
-    
-    setMenuOpen(false);
   };
   
   useEffect(() => {
-    document.addEventListener('mousedown', closeMenu);
+    if (isMenuOpen) {
+      document.addEventListener('mousedown', closeMenu);
+    } else {
+      document.removeEventListener('mousedown', closeMenu);
+    }
 
     return () => {
       document.removeEventListener('mousedown', closeMenu);
     };
-  }, []);
+  }, [isMenuOpen]);
 
   return (
     <div className='header-admin'>
-      <div className="menu-button-admin" onClick={toggleMenu}>
+      <div className="menu-button-admin" ref={buttonRef} onClick={toggleMenu}>
         ☰
       </div>
       <section className="head-1-admin">
@@ -40,7 +45,7 @@ const HeaderAdmin: React.FC = () => {
             Rastro Urbano
           </a>
         </h1>
-        <nav className={isMenuOpen ? 'open' : ''}>
+        <nav ref={menuRef} className={isMenuOpen ? 'open' : ''}>
           <ul className="lista-direita-admin">
             <li><a href={`/admuser/${id}`}>Início</a></li>
             <li><a href={`/admuser/${id}/perfil`}>Meu Perfil</a></li>
