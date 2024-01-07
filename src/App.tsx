806365d23a72d12118d5055ae55d110fdf5ab38c
Prado <peu_prado01@hotmail.com>
2024-01-07 13:43:59 -0300

    fix: pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/inicialPages/Home';
 import Sobre from './pages/inicialPages/About';
-import ProfilePage from './pages/Profile';
+import ProfilePage from './components/ProfileComponent';
 import ArtList from './pages/inicialPages/Art';
-import ArtPage from './pages/ArtPage';
+import ArtPage from './components/ArtPage';
 import Login from './pages/inicialPages/Login';
 import AdmUser from './pages/admPages/AdmUser';
 import ProfileAdmin from './pages/admPages/ProfileAdmin';
 import ArtistAdmin from './pages/admPages/ArtistAdmin';
 import ArtAdmin from './pages/admPages/ArtAdmin';
-import ProfileAdminPost from './pages/ProfilePost';
-import ProfilePageEdit from './pages/ProfileEdit';
+import ProfileAdminPost from './pages/admPages/ProfileAdminPost';
+import ProfilePageEdit from './components/ProfilePageComponent';
 import Artist from './pages/inicialPages/Artist';
-import ArtPageEdit from './pages/ArtPost';
-import ProfileArtist from './pages/ProfileArtist';
+import ArtPageEdit from './pages/admPages/ArtPagePostAdmin';
+import ProfileArtist from './pages/artistPages/ProfileArtist';
 import AdmArtist from './pages/artistPages/AdmArtist';
 import ProfileArtistEdit from './pages/artistPages/ProfileArtistEdit';
 import ProfileArtistPost from './pages/artistPages/ProfileArtistPost';
@@ -39,7 +39,7 @@ const AppRouter: React.FC = () => {
         <Route path="/admuser/:id/artes" element={<ArtAdmin />} />
         <Route path="/admuser/:id/profilepost" element={<ProfileAdminPost />} />
         <Route path="/admuser/:userId/admuser/:id/perfiladm" element={<ProfilePageEdit/>} />
-        <Route path="/admuser/:id/artedit" element={<ArtPageEdit/>} />
+        <Route path="/admuser/:id/artepost" element={<ArtPageEdit/>} />
         <Route path="/profileartist/:id" element={<ProfileArtist />} />
         <Route path="/admartist/:id" element={<AdmArtist />} />
         <Route path="/admartist/:id/profile" element={<ProfileArtistEdit />} />
