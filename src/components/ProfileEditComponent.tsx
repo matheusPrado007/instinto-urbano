63bfb2ddc67d30247dafcfb6459c9fd47b16cde5
Prado <peu_prado01@hotmail.com>
2024-01-26 06:40:32 -0300

    feat: edição de perfil de artistas por usuario root

--- src/components/ProfileEditComponent.tsx
+++ src/components/ProfileEditComponent.tsx
@@ -21,7 +21,7 @@ interface User {
 
 const ProfileEditComponent: React.FC = () => {
     const { fazerLogin, dadosUsers, enviarDadosParaBackend, deleteUsuario } = useApi();
-    const { id } = useParams<{ id?: string }>();
+    const { id, userId } = useParams<{ id?: string, userId?: string }>();
     const navigate = useNavigate();
 
     const [showPopup, setShowPopup] = useState(false);
@@ -154,8 +154,14 @@ const ProfileEditComponent: React.FC = () => {
     };
 
     useEffect(() => {
+        const urlAtual = window.location.href;
+        let foundUser;
         if (id) {
-            const foundUser = dadosUsers.find((u) => u._id === id);
+            if (urlAtual.includes(`perfilartistaedit`)) {
+                foundUser = dadosUsers.find((u) => u._id === userId);
+            } else {
+                foundUser = dadosUsers.find((u) => u._id === id);
+            }
 
             if (foundUser) {
                 const emailStorage: string = foundUser.email;
--- src/pages/admPages/ArtistAdmin.tsx
+++ src/pages/admPages/ArtistAdmin.tsx
@@ -1,64 +1,149 @@
-// AdmUser.tsx
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import { useApi } from '../../services/context/ApiContext';
 import Footer from '../../components/FooterComponent';
 import HeaderAdmin from '../../components/HeaderAdmin';
 import Header from '../../components/HeaderComponent';
 import Loading from '../../components/LoadingComponent';
+import ArtistList from '../../components/ArtistListComponent';
+import { CustomNextArrow, CustomPrevArrow } from '../../components/BtnComponent';
+import Slider from 'react-slick';
 
-const ArtistAdmin: React.FC = () => {
-  const { fazerLogin, dadosArtes } = useApi();
-  const [isLoading, setIsLoading] = useState(true); 
+
+const Artist: React.FC = () => {
+  const { dadosUsers } = useApi();
+  const navigate = useNavigate();
+  const { id } = useParams<{ id?: string }>();
+  const [user, setUser] = useState<any>();
+  const [idParams, setIdPrams] = useState<any>();
+  const [larguraTotal, setLarguraTotal] = useState(100);
+  const [isLoading, setIsLoading] = useState(true)
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  const navigateToProfile = (userId: string) => {
+    navigate(`admuser/${userId}/perfilartistaedit`); 
+  }
+  
+    const adm = dadosUsers && dadosUsers.filter((user) => user.administrador === false)
 
   useEffect(() => {
-    
-    const timeout = setTimeout(() => {
-      setIsLoading(false);
-    }, 1300);
+    setIdPrams(id);
+  }, [id])
 
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, []);
-
-
-  const isAdmin = async ({ email, senha }: any) => {
-    try {
-      const { accessToken, refreshToken } = await fazerLogin({ email, senha });
-      if (accessToken && refreshToken) {
-        return true
-      }
-    } catch (error) {
-      console.error('Erro durante o login:', error);
-    }
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
   };
 
-  if (!isAdmin) {
-    return (
-      <>
-        <Header />
-        {isLoading && <Loading />}
-        <div className="content">
-          <p>Você não tem permissão para acessar esta página.</p>
-        </div>
-        <Footer />
-      </>
+
+ const filteredArtist = adm && adm.filter((artist) => {
+    const searchTermWithoutTilde = searchTerm.replace(/~/g, '');
+    const fieldValueWithoutTilde = artist.username.toUpperCase().replace(/~/g, '');
+
+    return fieldValueWithoutTilde.includes(searchTermWithoutTilde.toUpperCase()) || (
+      searchTermWithoutTilde.startsWith('~') &&
+      fieldValueWithoutTilde.includes(searchTermWithoutTilde.substring(1).toUpperCase())
     );
-  }
+  });
+
+  useEffect(() => {
+    const larguraDaTela = window.innerWidth;
+    const handleResize = () => {
+      const larguraOriginalDaImagem = 150;
+      const numeroMaximoDeImagens = 6;
+
+      const quantidadeDeImgsPorTela = larguraDaTela / larguraOriginalDaImagem;
+    
+      const numeroTotal = larguraDaTela / filteredArtist.length;
+      
+        const numeroDeImgsSemFiltro = Math.min(Math.floor(larguraDaTela / larguraOriginalDaImagem), numeroMaximoDeImagens);
+    
+
+      console.log('numeroTotal',+numeroTotal.toFixed(0));
+      const numero = Math.min(Math.floor(larguraDaTela / +numeroTotal.toFixed(0)), numeroMaximoDeImagens);
+      console.log('num',numero);
+      
+      const realNumero = filteredArtist.length < quantidadeDeImgsPorTela ? numero : numeroDeImgsSemFiltro;
+      console.log('realNumero', realNumero);
+      
+  
+      setLarguraTotal(realNumero);
+    };
+  
+    window.addEventListener('resize', handleResize);
+    handleResize();
+  
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+  
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      clearTimeout(timeout);
+    };
+  }, [filteredArtist.length, window.innerWidth]);
+
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: larguraTotal,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+  };
 
   return (
     <>
-      <HeaderAdmin />
-      {isLoading && <Loading />}
-      {dadosArtes.length <= 0 && <Loading />} 
-      <div className="content">
-        <h2>Página do Administrador - Artista</h2>
+    <HeaderAdmin />
+    <div className="input-container-adm">
 
+      <input
+        type="text"
+        className="inputField-adm-artist"
+        placeholder={`🔍  Pesquisar por Artistas... `}
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      
       </div>
-      <Footer />
-    </>
+      {!filteredArtist || filteredArtist.length === 0 && <p className="galeria-item">Nenhuma Artista encontrado</p>}
+    <div className="user-list-container-artist">
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <p className="user-list-header-artist">Conheça os Artistas</p>
+          <div className='galeria-artist-list-container'>
+            <Slider {...settings} className='galeria'>
+              {filteredArtist.map((item: any) => (
+                <div key={item._id}  className="galeria-item"  onClick={() => navigateToProfile(item._id)}>
+                  <img
+                    src={item.foto_perfil}
+                    className="user-avatar-artist"
+                    alt={`Arte de ${item.username}`}
+                  />
+                  <p className='nome-user-artist'>{item.username}</p>
+                  <span className='nome-user-artist'>
+                    {item.descricao_curta}
+                  </span>
+                </div>
+              ))}
+            </Slider>
+          </div>
+        </>
+      )}
+    </div>
+    <Footer />
+  </>
+  
   );
 };
 
-export default ArtistAdmin;
+
+export default Artist;
